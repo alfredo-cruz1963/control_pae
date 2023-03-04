b037f79498738604e8e2a698676f10184874bd87
Alfredo <alfredogutierrezcruzu@hotmail.com>
2023-03-04 12:52:43 -0500

    25 subida

--- src/keys.js
+++ src/keys.js
@@ -1,27 +1,12 @@
-//const config = require('./config');
+const config = require('./config');
 
 module.exports = {
-
- 
-  database: {
-          connectionLimit: 5000,
-          host: 'localhost',
-          user: 'root',
-          password: 'root',
-          database: 'web_db_pae'
-      }  
-  
- 
- /*      
   database: {
     connectionLimit: 1000,
-    host: 'suditecsas.com',
-    user: 'u462945902_PAE',
-    password: 'Pae@2023',
-    database: 'u462945902_PAE',
-    DB_PORT: 3306
-  } 
-  */
-
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database,
+    port: process.env.DB_PORT
+  }
 };
-
---
+++ src/keys1.js
@@ -0,0 +1,27 @@
+//const config = require('./config');
+
+module.exports = {
+
+ 
+  database: {
+          connectionLimit: 5000,
+          host: 'localhost',
+          user: 'root',
+          password: 'root',
+          database: 'web_db_pae'
+      }  
+  
+ 
+ /*      
+  database: {
+    connectionLimit: 1000,
+    host: 'suditecsas.com',
+    user: 'u462945902_PAE',
+    password: 'Pae@2023',
+    database: 'u462945902_PAE',
+    DB_PORT: 3306
+  } 
+  */
+
+};
+
